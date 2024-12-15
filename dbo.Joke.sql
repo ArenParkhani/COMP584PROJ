2e3083a693b5e430bf94d0cbd62053f4ed9bace0
ArenParkhani <98431398+ArenParkhani@users.noreply.github.com>
2024-12-14 23:17:39 -0800

    D

--- dbo.Joke.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Joke] (
-    [Id]           INT            IDENTITY (1, 1) NOT NULL,
-    [JokeQuestion] TEXT            NULL,
-    [JokeAnswer]   TEXT NULL,
-    CONSTRAINT [PK_Joke] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
